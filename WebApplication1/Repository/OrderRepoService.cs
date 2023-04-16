d7c44ca607ae2a650b748e0181c6ef4f1a7a8a3a
Shaza Usama <Shaza Usama@LAPTOP-SG0CGNFN>
2023-04-16 17:39:44 +0200

    Added OrderRepo

--- WebApplication1/Repository/OrderRepoService.cs
+++ WebApplication1/Repository/OrderRepoService.cs
@@ -1,6 +1,40 @@
-﻿namespace WebApplication1.Repository
+﻿using WebApplication1.Data;
+using WebApplication1.Models;
+
+namespace WebApplication1.Repository
 {
-    public class OrderRepoService
+    public class OrderRepoService: IRepository<Order>
     {
+        public ElDbContext Context { get; set; }
+
+        public OrderRepoService(ElDbContext context)
+        {
+            Context = context;
+        }
+
+        public List<Order> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Order? GetDetails(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Insert(Order t)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(int id, Order t)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Delete(int id)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
